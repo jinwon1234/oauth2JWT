3757ab0d06f3642bb8944d5a8d2e21a3f8649c11
s202042170 <202042170@pusan.ac.kr>
2025-06-07 17:51:39 +0900

    https 로 변경

--- src/main/java/com/oauth2/jwt/config/SecurityConfig.java
+++ src/main/java/com/oauth2/jwt/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("http://www.jinwon.click"));
+                        configuration.setAllowedOrigins(Collections.singletonList("https://www.jinwon.click"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
